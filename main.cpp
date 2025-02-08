98f1e9a71a3688a11d026640cd6743958ced60dc
Zoeyy1007 <zoeyy645@gmail.com>
2025-02-07 20:17:19 -0800

    userinterface

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void Main_menu(){
+    cout << "1. Add event" << endl;
+    cout << "2. View my schedule" << endl;
+}
+
+int main(){
+    int option = 0;
+    Main_menu();
+    
+}
