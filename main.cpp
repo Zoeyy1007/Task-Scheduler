c5023c79d2a2bc83d78edec257747f1a6d4bf11c
ctraino <jcitr001@xe-01.cs.ucr.edu>
2025-02-26 23:36:15 -0800

    Rough finish of ui, need to add more options and finish schedule implementation

--- main.cpp
+++ main.cpp
@@ -56,21 +56,55 @@ int main(){
                 s->display_full();
             }
             else if(op2choice1 == "2"){
-                string catname;
-
+                s->display_by_category();
             }
+            else if(op2choice1 == "3"){
+                //s->display_by_day();
+            }
+        Main_menu();
+        cin >> option;
 
 
 
         }
         else if(option == "3"){
+            string eventName;
+            cout << "Type name of task" << endl;
+            getline(cin, eventName);
+            Task temp = s->findTask(eventName);
+
+
+
+
+            Edit_event();
+            string op3choice1;
+            cin >> op3choice1;
+            if(op3choice1 == "1") {
+                //s->edit_event_title();  
+            } else if(op3choice1 == "2") {
+               // s->edit_event_time();  
+            } else if(op3choice1 == "3") {
+                //s->edit_event_day();  
+            } else if(op3choice1== "4") {
+                //s->delete_event();  
+            } else if(op3choice1 == "5") {
+                //s->mark_as_completed();  
+            } else if(op3choice1 == "q") {
+                cout << "Returning to main menu" << endl;
+                option = "q";
+            } else {
+                cout << "Invalid choice, try again." << endl;
+            }
+
+
 
         }
         else if(option == "4"){
-
-        }
-        else if(option == "5"){
-
+            cout << "What priority level would you want to view?" << endl;
+            int prio;
+            cin >> prio;
+            //s->display_priority(prio);
+            Main_menu();
         }
         else{
             cout << "seems like you didnt pick something, pick again" << endl << endl;
@@ -79,21 +113,21 @@ int main(){
         }
     }
     cout << "Thank you for using our program!" << endl;
-
+    delete s;
     return 0;
 }
 
 void Main_menu(){
     cout << "1. Add event" << endl;
     cout << "2. View my schedule" << endl;
-    cout << "3. Remove event" << endl;
-    cout << "4. Edit event" << endl;
-    cout << "5. View Priority" << endl;
+    cout << "3. Edit event" << endl;
+    cout << "4. View Priority" << endl;
 }
 
 void Sched_options(){
     cout << "1. View full Schedule" << endl;
     cout << "2. View Schedule by category" << endl;
+    cout << "3. View schedule by day." << endl;
 }
 
 
@@ -101,15 +135,9 @@ void Edit_event(){
     cout << "Edit options: " << endl;
     cout << "1. Edit event title" << endl;
     cout << "2. Edit event time" << endl;
-    cout << "3. Delete this event" << endl;
-    cout << "4. Mark as completed" << endl;
-    cout << "Enter 'q' to go back to the previous menu" << endl;
+    cout << "3. Edit event day" << endl;
+    cout << "4. Delete this event" << endl;
+    cout << "5. Mark as completed" << endl;
+    cout << "Enter 'q' to go back to the main menu" << endl;
 }
-
-void display_option(){
-    cout << "How do you like to view your schedule?" << endl;
-    cout << "Enter 1 to view by time. " << endl;
-    cout << "Enter 2 to view by catogory" << endl;
-}
-
 
