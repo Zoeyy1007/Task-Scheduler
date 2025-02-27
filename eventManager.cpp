1ed93a24d75fa44a12cdef236e023fd5acc13d42
Taha Mohiuddin <tmohi001@xe-01.cs.ucr.edu>
2025-02-26 17:42:31 -0800

    readded eventManager.cpp

---
+++ eventManager.cpp
@@ -0,0 +1,47 @@
+#include "EventManager.h"
+
+using namespace std;
+
+// constructor
+EventManager::EventManager(const string& eventName, int start, int dur) 
+    : name(eventName), startTime(start), duration(dur) {}
+//allows to set length of time for each event
+
+string EventManager::getName() const {
+    return name;
+}
+
+int EventManager::getStartTime() const {
+    return startTime;
+}
+
+int EventManager::getEndTime() const {
+    return startTime + duration;  
+}
+
+int EventManager::getDuration() const {
+    return duration;
+}
+
+// checks for time conflict 
+bool EventManager::hasConflict(const EventManager& newEvent, const vector<EventManager>& events) {
+    for (const EventManager& event : events) {  
+        int existingStart = event.getStartTime();
+        int existingEnd = event.getEndTime();
+        int newStart = newEvent.getStartTime();
+        int newEnd = newEvent.getEndTime();
+
+        if (newStart < existingEnd && newEnd > existingStart) {
+            cout << "Conflict with: " << event.getName() << endl;
+            return true;
+        }
+    }
+    return false;
+}
+void EventManager::displayEvent() const {
+    cout << "Event: " << name 
+         << " Start Time: " << startTime 
+         << " Duration: " << duration 
+         << " minutes. Ends at: " << getEndTime() << endl;
+}
+
