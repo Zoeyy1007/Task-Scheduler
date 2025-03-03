00e3bdf4c7073b3e803b001d6118fa8426968ba2
LydaTaing <tainglyda@gmail.com>
2025-03-02 17:56:33 -0800

    add setter function

--- task.h
+++ task.h
@@ -7,14 +7,36 @@ class Task {
     private:
     string task_name;
     int task_priority = 0;
-    int task_date = 0;
+    string task_date;
     int task_exec_time = 0;
+    int task_day;
+    int task_hour;
+    int task_complete;
+
     public:
-    bool task_complete = 0;
+
+    Task(string taskname, string task_date, int priority, string category, int day, int hour)
+    
+    //getters
+    bool get_status();
     string getName();
     int getPriority();
-    int getDate();
+    string getDate();
     int get_exec_time();
+    int get_day()
+    int get_hour();
+    string get_category();
+
+    //setter
+    void set_name(string newName);
+    void set_date(string newDate);
+    void set_priority(int newPriority);
+    void set_category(string newCategory);
+    void set_day(int newDay);
+    void set_hour(int newHour);
+
+    //completion
+    void task_complete();
 };
 
 
